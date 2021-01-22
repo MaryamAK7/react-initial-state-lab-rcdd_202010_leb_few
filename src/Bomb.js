9fdf17198a4e9b2e59fe90e16f0c7d4e8dbaf325
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-22 19:20:16 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,7 +2,10 @@
 import React, { Component } from 'react';
 
 export default class Bomb extends Component {
-  
+  constructor(props){
+    super()
+    
+  }
   render() {
     return(
       <div> </div>)
