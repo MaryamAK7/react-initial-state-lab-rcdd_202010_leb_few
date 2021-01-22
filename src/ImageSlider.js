de91063c2eaaded999c7c7d727d45390ec98e4b4
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-22 19:17:48 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 
 export default class ImageSlider extends Component {
   
+  constructor() {
+    super() 
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
   render() {
-    return()
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
   }
   
 }
